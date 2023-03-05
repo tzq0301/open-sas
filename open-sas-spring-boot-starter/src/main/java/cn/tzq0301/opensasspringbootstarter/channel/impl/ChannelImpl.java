13d2fba407ba75e1c30fd31d0a003ec866f74f54
tzq0301 <1661610563@qq.com>
2023-03-05 17:43:48 +0800

    解决 ChannelImpl 中对并发临界区的并发 bug

--- open-sas-spring-boot-starter/src/main/java/cn/tzq0301/opensasspringbootstarter/channel/impl/ChannelImpl.java
+++ open-sas-spring-boot-starter/src/main/java/cn/tzq0301/opensasspringbootstarter/channel/impl/ChannelImpl.java
@@ -25,7 +25,7 @@ public final class ChannelImpl implements Channel {
     }
 
     @Override
-    public synchronized void registerSubscriber(@NonNull final Group group,
+    public void registerSubscriber(@NonNull final Group group,
                                                 @NonNull final Version version,
                                                 @NonNull final Priority priority,
                                                 @NonNull final Map<Topic, SubscriberCallback> topicToCallbackMap) {
@@ -34,84 +34,90 @@ public final class ChannelImpl implements Channel {
         checkNotNull(priority);
         checkNotNull(topicToCallbackMap);
 
-        if (!groupMap.containsKey(group)) {
-            groupMap.put(group, Maps.newHashMap());
-        }
-
-        var versionMap = checkNotNull(groupMap.get(group));
-        if (!versionMap.containsKey(version)) {
-            versionMap.put(version, Maps.newHashMap());
-        }
-
-        var topicMap = checkNotNull(versionMap.get(version));
-        topicToCallbackMap.forEach((topic, subscriberCallback) -> {
-            if (!topicMap.containsKey(topic)) {
-                topicMap.put(topic, Maps.newTreeMap());
+        synchronized (this) {
+            if (!groupMap.containsKey(group)) {
+                groupMap.put(group, Maps.newHashMap());
             }
-            var priorityMap = checkNotNull(topicMap.get(topic));
-            checkArgument(!priorityMap.containsKey(priority), "The subscriber with same group (%s), same version (%s), same priority (%), same topic(%s)", group, version, priority, topic);
-            priorityMap.put(priority, subscriberCallback);
-        });
+
+            var versionMap = checkNotNull(groupMap.get(group));
+            if (!versionMap.containsKey(version)) {
+                versionMap.put(version, Maps.newHashMap());
+            }
+
+            var topicMap = checkNotNull(versionMap.get(version));
+            topicToCallbackMap.forEach((topic, subscriberCallback) -> {
+                if (!topicMap.containsKey(topic)) {
+                    topicMap.put(topic, Maps.newTreeMap());
+                }
+                var priorityMap = checkNotNull(topicMap.get(topic));
+                checkArgument(!priorityMap.containsKey(priority), "The subscriber with same group (%s), same version (%s), same priority (%), same topic(%s)", group, version, priority, topic);
+                priorityMap.put(priority, subscriberCallback);
+            });
+        }
     }
 
     @Override
-    public synchronized void unregisterSubscriber(@NonNull final Group group,
+    public void unregisterSubscriber(@NonNull final Group group,
                                                   @NonNull final Version version,
                                                   @NonNull final Priority priority) {
         checkNotNull(group);
         checkNotNull(version);
         checkNotNull(priority);
 
-        checkArgument(groupMap.containsKey(group), "Group (%s) is not exists", group);
+        synchronized (this) {
+            checkArgument(groupMap.containsKey(group), "Group (%s) is not exists", group);
 
-        var versionMap = checkNotNull(groupMap.get(group));
-        checkArgument(versionMap.containsKey(version), "Version (%s) is not exists", version);
+            var versionMap = checkNotNull(groupMap.get(group));
+            checkArgument(versionMap.containsKey(version), "Version (%s) is not exists", version);
 
-        var topicMap = checkNotNull(versionMap.get(version));
-        topicMap.forEach((topic, prioritySubscriberCallbackNavigableMap) -> {
-            var priorityMap = checkNotNull(topicMap.get(topic));
-            // remove subscribe
-            priorityMap.remove(priority);
-        });
+            var topicMap = checkNotNull(versionMap.get(version));
+            topicMap.forEach((topic, prioritySubscriberCallbackNavigableMap) -> {
+                var priorityMap = checkNotNull(topicMap.get(topic));
+                // remove subscribe
+                priorityMap.remove(priority);
+            });
 
-        topicMap.entrySet().removeIf(entry -> entry.getValue().isEmpty());
+            topicMap.entrySet().removeIf(entry -> entry.getValue().isEmpty());
 
-        // dynamically remove topicMap if empty
-        if (topicMap.isEmpty()) {
-            versionMap.remove(version);
-        }
+            // dynamically remove topicMap if empty
+            if (topicMap.isEmpty()) {
+                versionMap.remove(version);
+            }
 
-        // dynamically remove versionMap if empty
-        if (versionMap.isEmpty()) {
-            groupMap.remove(group);
+            // dynamically remove versionMap if empty
+            if (versionMap.isEmpty()) {
+                groupMap.remove(group);
+            }
         }
     }
 
     @Override
-    public synchronized void publish(@NonNull final Group group,
+    public void publish(@NonNull final Group group,
                                      @NonNull final Version version,
                                      @NonNull final Priority priority,
                                      @NonNull final Topic topic,
                                      @NonNull final Message message) {
         checkNotNull(message);
 
-        if (!groupMap.containsKey(group)) {
-            return;
-        }
+        synchronized (this) {
+            if (!groupMap.containsKey(group)) {
+                return;
+            }
 
-        var versionMap = checkNotNull(groupMap.get(group));
-        if (!versionMap.containsKey(version)) {
-            return;
-        }
+            var versionMap = checkNotNull(groupMap.get(group));
+            if (!versionMap.containsKey(version)) {
+                return;
+            }
 
-        var topicMap = checkNotNull(versionMap.get(version));
-        if (!topicMap.containsKey(topic)) {
-            return;
-        }
+            var topicMap = checkNotNull(versionMap.get(version));
+            if (!topicMap.containsKey(topic)) {
+                return;
+            }
 
-        var priorityMap = checkNotNull(topicMap.get(topic));
-        Optional.ofNullable(priorityMap.floorEntry(priority))
-                .map(Map.Entry::getValue)
-                .ifPresent(callback -> callback.onMessage(topic, message));
+            var priorityMap = checkNotNull(topicMap.get(topic));
+            Optional.ofNullable(priorityMap.floorEntry(priority))
+                    .map(Map.Entry::getValue)
+                    .ifPresent(callback -> callback.onMessage(topic, message));
+        }
     }
 }
